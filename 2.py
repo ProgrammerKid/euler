13c612dafe22b061d82289f85a80cfe99f189732
Viraj Shah <shahvirajd@yahoo.com>
2015-01-30 21:17:42 -0500

    Finished with 2.py, and made fib modules in list.py

---
+++ 2.py
@@ -0,0 +1,7 @@
+import lists
+
+fibs = lists.generateFib(4000000)
+fibs = lists.removeOdds(fibs)
+fibs = lists.add(fibs)
+
+print fibs
--- lists.py
+++ lists.py
@@ -10,8 +10,31 @@ def clearRepeats(array):
 			newArray.append(element)
 	return newArray
 
-def addAll(array):
+def add(array):
 	sums = 0;
 	for element in array:
 		sums = sums + element
 	return sums
+
+def generateFib(till):
+	'''
+	1, 2, 3, 5, 8, 13
+	1  2  3  4  5  6
+	'''
+	sequence = [1, 2, 3]
+	while sequence[len(sequence) - 1] <= till:
+		lastNum = sequence[len(sequence) - 1]
+		secondToLastNum = sequence[len(sequence) - 2]
+		total = lastNum + secondToLastNum
+		sequence.append(total)
+	#evaluating the sequence
+	if sequence[len(sequence) - 1] > till:
+		sequence.pop()
+	return sequence
+
+def removeOdds(array):
+	newArray = []
+	for element in array:
+		if not element % 2: #if element is even
+			newArray.append(element)
+	return newArray
